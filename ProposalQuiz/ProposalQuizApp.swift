99d65a37e46e528150661b959409e497b1ce900c
Russell Blickhan <rwblickhan@gmail.com>
2021-12-18 21:36:46 -0800

    Initial Commit

---
+++ ProposalQuiz/ProposalQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProposalQuizApp.swift
+//  ProposalQuiz
+//
+//  Created by Russell Blickhan on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ProposalQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
